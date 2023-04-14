fa4a2c349c8105409e5b896d6368baf080b5179b
narinderCanGit <narinder111198@gmail.com>
2023-04-14 13:14:06 +0530

    CRUD operation

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost/AlienDB' 
+
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...')
+})
+
+// con.on('error',()=>{
+//     console.log("Error in connecting to the database");
+// })
+
+app.use(express.json())
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
+
+// 127.0.0.1:9000/aliens/about
+
+
+app.listen(8000, () => {
+    console.log('Server started on port')
+})
